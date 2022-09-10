6b26dd8ef082c265fab9d4641c7faf9115293f14
Fedecardozo <Fedecardozo.a@gmail.com>
2022-09-10 16:22:17 -0300

    windowForm ejercicio 2

--- Clase06_Ejer02/FormPrincipal.Designer.cs
+++ Clase06_Ejer02/FormPrincipal.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Clase06_Ejer02
 {
-    partial class Form1
+    partial class FormPrincipal
     {
         /// <summary>
         ///  Required designer variable.
@@ -30,9 +30,9 @@ namespace Clase06_Ejer02
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.textNumero = new System.Windows.Forms.TextBox();
+            this.lstTabla = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // label1
@@ -45,44 +45,45 @@ namespace Clase06_Ejer02
             this.label1.TabIndex = 0;
             this.label1.Text = "Ingrese un numero:";
             // 
-            // button1
+            // btnLimpiar
             // 
-            this.button1.Location = new System.Drawing.Point(117, 380);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(156, 34);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Limpiar";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnLimpiar.Location = new System.Drawing.Point(117, 380);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(156, 34);
+            this.btnLimpiar.TabIndex = 1;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox1
+            // textNumero
             // 
-            this.textBox1.Location = new System.Drawing.Point(200, 45);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(119, 27);
-            this.textBox1.TabIndex = 2;
+            this.textNumero.Location = new System.Drawing.Point(200, 45);
+            this.textNumero.Name = "textNumero";
+            this.textNumero.Size = new System.Drawing.Size(119, 27);
+            this.textNumero.TabIndex = 2;
+            this.textNumero.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textNumero_KeyPress);
             // 
-            // listBox1
+            // lstTabla
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 20;
-            this.listBox1.Location = new System.Drawing.Point(55, 122);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(264, 224);
-            this.listBox1.TabIndex = 3;
-            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.lstTabla.FormattingEnabled = true;
+            this.lstTabla.ItemHeight = 20;
+            this.lstTabla.Location = new System.Drawing.Point(55, 122);
+            this.lstTabla.Name = "lstTabla";
+            this.lstTabla.Size = new System.Drawing.Size(264, 224);
+            this.lstTabla.TabIndex = 3;
             // 
-            // Form1
+            // FormPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(366, 462);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lstTabla);
+            this.Controls.Add(this.textNumero);
+            this.Controls.Add(this.btnLimpiar);
             this.Controls.Add(this.label1);
-            this.Name = "Form1";
+            this.Name = "FormPrincipal";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormPrincipal_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -92,9 +93,9 @@ namespace Clase06_Ejer02
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.TextBox textNumero;
+        private System.Windows.Forms.ListBox lstTabla;
     }
 }
 
---
+++ Clase06_Ejer02/FormPrincipal.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clase06_Ejer02
+{
+    public partial class FormPrincipal : Form
+    {
+        public FormPrincipal()
+        {
+            //Inicia el formulario, es parecido al constructor
+            InitializeComponent();
+
+            //Centra el formulario
+            this.StartPosition = FormStartPosition.CenterScreen;
+      
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            this.Text = "Tabla";
+
+            //this.Text = "Cambia nombre del titulo del formulario";
+
+            //Limpia el list box
+            //this.listBox1.ClearSelected();
+
+            //Agrega  un item
+            //this.listBox1.Items.Add("Item ");
+
+            //Cambia color de fondo
+            //this.BackColor = Color.Red;
+
+            //Cambia color de la letra de todo el form
+            //this.ForeColor = Color.BlueViolet;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Para mostrar mensajes en modo alerta, modificar iconos y muchas cosas mas
+            //MessageBox.Show("Hola mundo","Hola",MessageBoxButtons.OK,MessageBoxIcon.Stop);
+
+            this.lstTabla.Items.Clear();
+            this.textNumero.Text = "";
+
+        }
+
+        private void textNumero_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Muestra la tecla presionada
+            //MessageBox.Show(e.KeyChar.ToString());
+
+            if(e.KeyChar == (char)Keys.Enter)
+            {
+                //Agrega  un item y desarrollar un metodo para que haga la tabla de multiplicar
+                this.multiplicar(this.textNumero.Text);
+            }
+        }
+
+        private void multiplicar(string numero)
+        {
+            for(int i=1; i<11; i++)
+            {
+                this.lstTabla.Items.Add($"{numero} x {i} = {i*int.Parse(numero)}");
+            }
+        }
+
+        private void FormPrincipal_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //if(MessageBox.Show("¿Realmente desea cerrar el formulario?","Exit",MessageBoxButtons.YesNo)==DialogResult.No)
+            if (MessageBox.Show("¿Realmente desea cerrar el formulario?", "Exit", MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question,MessageBoxDefaultButton.Button2) == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
