1e0d9fc78f89b7ae6ebc1c7c60a435168df8f46b
Fedecardozo <Fedecardozo.a@gmail.com>
2022-09-14 10:10:20 -0300

    Ejercicio 1 Tempera terminado, clase 7

--- Clase05/Program.cs
+++ Clase05/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using MiBiblioteca;
 
 namespace Clase05
 {
@@ -6,7 +7,67 @@ namespace Clase05
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*Crear 4 temperas:
+            1) Red-Alba-50
+            2) Blue-Alba-50
+            3) Red-Alba-25
+            4) Black-Pinturita-50
+            5) Green-Temperita-50*/
+
+            Tempera[] temperas = new Tempera[5];
+            temperas[0] = new Tempera(ConsoleColor.Red, "Alba", 50);
+            temperas[1] = new Tempera(ConsoleColor.Blue, "Alba", 50);
+            temperas[2] = new Tempera(ConsoleColor.Red, "Alba", 25);
+            temperas[3] = new Tempera(ConsoleColor.Black, "Pinturita", 50);
+            temperas[4] = new Tempera(ConsoleColor.Green, "Temperita", 50);
+
+            #region Comparacion temperas
+            /*Comparar (temperas):
+            1) == 2)
+            1) == 3)
+            1) == 4)
+            1) == null
+            null == 1)*/
+
+            Console.WriteLine("*** Comparacion de temeras ***\n");
+            Console.WriteLine(temperas[0] == temperas[1]);
+            Console.WriteLine(temperas[0] == temperas[2]);
+            Console.WriteLine(temperas[0] == temperas[3]);
+            Console.WriteLine(temperas[0] == null);
+            Console.WriteLine(null == temperas[0]);
+
+            #endregion
+
+            /*Sumar (temperas):
+            1) + 2)
+            1) + 3)
+            1) + 4)
+            1) + null*/
+
+            Console.WriteLine("\n*** Sumas ***");
+
+            Tempera aux;
+
+            aux = temperas[0] + temperas[1];
+            Console.WriteLine("1) " + (int)aux);
+            aux = temperas[0] + temperas[2];
+            Console.WriteLine("2) " + (int)aux);
+            aux = temperas[0] + temperas[3];
+            Console.WriteLine("3) " + (int)aux);
+            aux = temperas[0] + temperas[4];
+            Console.WriteLine("4) " + (int)aux);
+
+
+
+            /*Sumar (temperas):
+            1) + 20
+            2) + 30*/
+            Console.WriteLine("Sumar temperas por cantidad");
+            aux = (temperas[0] + 20);
+            Console.WriteLine("1) " +(int)aux);
+            aux = (temperas[0] + 30);
+            Console.WriteLine("2) " +(int)aux);
+
         }
     }
 }
---
+++ MiBiblioteca/Tempera.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiBiblioteca
+{
+    public class Tempera
+    {
+        private ConsoleColor color;
+        private String marca;
+        private Int32 cantidad;
+
+        #region Constructores
+
+        public Tempera()
+        {
+            this.color = ConsoleColor.Black;
+            this.marca = "";
+            this.cantidad = 0;
+        }
+
+        public Tempera(ConsoleColor color) :this()
+        {
+            this.color = color;
+        }
+
+        public Tempera(ConsoleColor color, String marca) :this(color)
+        {
+            this.marca = marca;
+        }
+
+        public Tempera(ConsoleColor color, String marca, Int32 cantidad) :this(color,marca)
+        {
+            this.cantidad = cantidad;
+        }
+
+        #endregion
+
+        #region Metodos
+
+        private string Mostar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Color: {this.color}");
+            sb.AppendLine($"Marca: {this.marca}");
+            sb.AppendLine($"Cantidad: {this.cantidad}");
+
+            return sb.ToString();
+        }
+
+        public static string Mostrar(Tempera tempera)
+        {
+            return tempera.Mostar();
+        }
+
+        #endregion
+
+        #region Sobrecarga Operadores
+        public static bool operator ==(Tempera tempera1, Tempera tempera2)
+        {
+            bool retorno = false;
+
+            if (tempera1 is null && tempera2 is null)
+                retorno = true;
+            else if(tempera1 is not null && tempera2 is not null )
+                retorno = tempera1.marca == tempera2.marca && tempera1.color == tempera2.color;
+
+            return retorno;
+        } 
+
+        public static bool operator !=(Tempera tempera1, Tempera tempera2)
+        {
+            return !(tempera1 == tempera2);
+        }
+
+        public static implicit operator int(Tempera tempera)
+        {
+            return tempera.cantidad;
+        }
+
+        public static Tempera operator +(Tempera tempera1, Tempera tempera2)
+        {
+            Tempera aux = new Tempera();
+
+            if (tempera1 == tempera2)
+                aux.cantidad = tempera1.cantidad + (int)tempera2;
+
+            return aux;
+        }
+
+        public static Tempera operator +(Tempera tempera1, int cantidad)
+        {
+            Tempera aux = new Tempera();
+
+            /*aux.cantidad = tempera1.cantidad + cantidad;
+
+            return aux; */
+
+            aux = tempera1;
+            aux.cantidad = cantidad;
+
+            return tempera1 + aux;
+
+        }
+
+        #endregion
+    }
+}
