9ec23a741f215254af681394093a9813bd375f1a
Fedecardozo <Fedecardozo.a@gmail.com>
2022-10-07 14:36:33 -0300

    Clase 3 ejercicio 2

---
+++ Clase03_Ejercicio02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using MiBiblioteca;
+
+namespace Clase03_Ejercicio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona1 = new Persona();
+            Persona persona2 = new Persona();
+            Persona persona3 = new Persona();
+
+            //Persona 1 por default
+
+            //Persona 2
+            persona2.Dni = 41623988;
+            persona2.FechaNacimiento = new DateTime(1998,11,28);
+            persona2.Nombre = "Claribelcita";
+
+            //Persona 3
+            persona3.Dni = 38853892;
+            persona3.FechaNacimiento = new DateTime(2010,3,5);
+            persona3.Nombre = "El Flaco";
+
+            Console.WriteLine(persona1.Mostrar());
+            Console.WriteLine(persona1.EsMayorDeEdad()+"\n");
+
+            Console.WriteLine(persona2.Mostrar());
+            Console.WriteLine(persona2.EsMayorDeEdad() + "\n");
+
+            Console.WriteLine(persona3.Mostrar());
+            Console.WriteLine(persona3.EsMayorDeEdad());
+
+        }
+    }
+}
---
+++ MiBiblioteca/Persona.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiBiblioteca
+{
+    public class Persona
+    {
+        private string nombre;
+        private DateTime fechaNacimento;
+        private int dni;
+
+        public Persona()
+        {
+            this.nombre = "";
+            this.fechaNacimento = new DateTime(1,1,1);
+            this.dni = 0;
+        }
+
+        public string Nombre 
+        {
+            get { return this.Nombre; }
+            set { this.nombre = value; }
+        }
+
+        public DateTime FechaNacimiento
+        {
+            get { return this.fechaNacimento; }
+            set { this.fechaNacimento = value; }
+        }
+
+        public int Dni
+        {
+            get { return this.dni; }
+            set { this.dni = value; }
+        }
+
+        private int CalcularEdad()
+        {
+            DateTime fechaActual = DateTime.Today;
+
+            return fechaActual.AddTicks(-this.fechaNacimento.Ticks).Year - 1;
+           
+        }
+
+        public string Mostrar()
+        {
+            return $"Nombre: {this.nombre} \nFecha nacimiento: {this.fechaNacimento}" +
+                $"\nEdad: {this.CalcularEdad()} \nDni: {this.dni}";
+        }
+
+        public string EsMayorDeEdad()
+        {
+            if (this.CalcularEdad() > 18)
+                return "Es mayor de edad";
+            else
+                return "Es menor de edad";
+        }
+
+    }
+}
