b0e17771ef73f1c5570db0a8a18f42a6f3454a19
Fedecardozo <Fedecardozo.a@gmail.com>
2022-10-07 21:20:02 -0300

    Sumador clase 04 ejercicio 1

---
+++ Clase04_Ejercicio1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MiBiblioteca;
+
+namespace Clase04_Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sumador sumador = new Sumador();
+            Sumador sumador2 = new Sumador(10);
+
+            long resultado = sumador + sumador2;
+
+            sumador.Sumar(10,15);
+
+            Console.WriteLine(resultado);
+            Console.WriteLine(sumador | sumador2);
+            Console.WriteLine("Cantidad de sumas:" + (int)sumador);
+        }
+    }
+}
---
+++ MiBiblioteca/Sumador.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiBiblioteca
+{
+    public class Sumador
+    {
+        private int cantidadSumas;
+
+        #region Constructores
+        public Sumador(int cantidadSumas)
+        {
+            this.cantidadSumas = cantidadSumas;
+        }
+
+        public Sumador() : this(0) {}
+
+        #endregion
+
+        #region Sobrecarga de operadores
+
+        public static explicit operator int (Sumador s)
+        {
+            return s.cantidadSumas;
+        }
+
+        public static bool operator |(Sumador s1, Sumador s2)
+        {
+            return s1.cantidadSumas == s2.cantidadSumas;
+        }
+
+        public static long operator +(Sumador s1, Sumador s2)
+        {
+            return s1.cantidadSumas + s2.cantidadSumas;
+        }
+
+        #endregion
+
+        #region Sobrecarga de metodos
+
+        private void  Sumar()
+        {
+            this.cantidadSumas++;
+        }
+
+        public long Sumar(long a, long b)
+        {
+            this.Sumar();
+            return a + b;
+        }
+
+        public string Sumar(string a, string b)
+        {
+            this.Sumar();
+            return a + b;
+        }
+        #endregion
+    }
+}
