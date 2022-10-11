4ab09b2bf2195edd1f78a3ae98db420ed739fa86
Fedecardozo <Fedecardozo.a@gmail.com>
2022-10-11 10:22:01 -0300

    TryParse propio

---
+++ ClaseExepciones/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ExeptionPropia;
+
+namespace ClaseExepciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.WriteLine("Ingrese un numero: ");
+            bool flag = ParseExeption.TryParse( Console.ReadLine(), out num);
+
+            if(!flag)
+            {
+                Console.WriteLine("No es un numero!");
+            }
+            else
+            {
+                Console.WriteLine("El numero es: " + num);
+            }
+
+        }
+
+    }
+ 
+}
---
+++ ExeptionPropia/ParseExeption.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ExeptionPropia
+{
+    public class ParseExeption
+    {
+        public static bool TryParse(string cadena, out int entero)
+        {
+            bool retorno = false;
+
+            try
+            {
+                entero = int.Parse(cadena);
+                retorno = true;
+            }
+            catch (Exception e)
+            {
+                entero = 0;
+            }
+
+            return retorno;
+        }
+    }
+}
